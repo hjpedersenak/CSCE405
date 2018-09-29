3fc72486551945eef7279a99977f9659d4783158
Hanna <hannajane.p@gmail.com>
2018-09-28 19:45:11 -0800

    Starting to work on simulated annealing

--- TravelingSalesperson/src/travelingsalesman/Annealing.java
+++ TravelingSalesperson/src/travelingsalesman/Annealing.java
@@ -2,7 +2,23 @@
 package travelingsalesman;
 
 
-public class Annealing 
-{
+public class Annealing {
+    
+    private double temp = 10000;
+    private double coolingRate = 0.003;
+    
+    public Annealing(CostGraph costgraph) {
+        
+    }
+    
+    public static double acceptanceProbability(int energy, int newEnergy, double temperature) {
+        if (newEnergy < energy) {
+            return 1.0;
+        }
+        return Math.exp((energy - newEnergy) / temperature);
+    }
+    
+    
+    
     
 }
