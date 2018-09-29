4a51971853e7680e2fa2d5947afb72bdbfa9096e
Olivia Putman <42879904+ojputman@users.noreply.github.com>
2018-09-28 18:07:14 -0800

    swapTest and calcCost methods

--- TravelingSalesperson/src/travelingsalesman/HillClimbing.java
+++ TravelingSalesperson/src/travelingsalesman/HillClimbing.java
@@ -1,8 +1,61 @@
 
 package travelingsalesman;
 
+import java.util.*;
+
 
 public class HillClimbing 
 {
+    private CostGraph hcGraph;
+    private ArrayList<Integer> cities;
+    private int numCities;
+    private int numRuns = 5;
+    
+    
+    public HillClimbing(CostGraph cg)
+    {
+        hcGraph = cg;
+        numCities = cg.getArrSize(); //make Tour class later
+        cities = new ArrayList(numCities);
+    }
+    
+    public int[] swapTest(ArrayList cities)
+    {
+        int[] bestSwap = new int[2];
+        int bestCost = 0;
+        for(int i = 1; i < numCities; i++)
+        {
+            for(int j = 1; j < numCities; j++)
+            {
+                if(i != j)
+                {
+                    ArrayList<Integer> testCase = cities;
+                    int temp = testCase.get(i);
+                    testCase.set(i, testCase.get(j));
+                    testCase.set(j, i);
+                    int testCost = calcCost(testCase);
+                    if(bestCost == 0 || bestCost > testCost)
+                    {
+                        bestCost = testCost;
+                        bestSwap[0] = i;
+                        bestSwap[1] = j;
+                    }
+                }
+            }
+        }
+        return bestSwap;
+    }
+    
+    public int calcCost(ArrayList testCase)
+    {
+        int cost = 0;
+        for(int i = 0; i < numCities - 1; i++)
+        {
+            cost = cost + hcGraph.getCost(i, i + 1);
+        }
+        cost = cost + hcGraph.getCost(numCities - 1, 0);
+        return cost;
+    }
     
 }
+
