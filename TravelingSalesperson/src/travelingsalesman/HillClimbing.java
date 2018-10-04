4e8d800fb2a45865cb6b73d16f12d510346a5663
Olivia Putman <42879904+ojputman@users.noreply.github.com>
2018-10-03 20:15:23 -0800

    got hillclimbing working
    added 5 auto runs and reporting to main method

--- TravelingSalesperson/src/travelingsalesman/HillClimbing.java
+++ TravelingSalesperson/src/travelingsalesman/HillClimbing.java
@@ -6,35 +6,51 @@ import java.util.*;
 
 public class HillClimbing 
 {
-    private CostGraph hcGraph;
-    private ArrayList<Integer> cities;
-    private int numCities;
-    private int numRuns = 5;
+    private final CostGraph hcGraph;
+    private ArrayList<Integer> tour;
+    private final int numCities;
     
     
     public HillClimbing(CostGraph cg)
     {
         hcGraph = cg;
-        numCities = cg.getArrSize(); //make into Tour class later
-        cities = new ArrayList(numCities);
+        numCities = cg.getArrSize(); 
     }
     
-    public int[] swapTest(ArrayList cities)
+    public int runTest(ArrayList newTour)
+    {
+        int newCost = 0;
+        this.tour = new ArrayList<Integer>(newTour);
+        boolean improved;
+        
+        do
+        {
+            improved = completeSwap(swapTest(tour));
+            newCost = calcCost(tour);
+        }
+        while(improved);
+        System.out.println("Final tour: " + tour + "\nFinal cost: " + newCost);
+        return newCost;
+    }
+    
+    public int[] swapTest(ArrayList<Integer> cities)
     {
         int[] bestSwap = new int[2];
-        int bestCost = 0;
+        bestSwap[0] = 0;
+        bestSwap[1] = 0;
+        int bestCost = calcCost(cities);
         for(int i = 1; i < numCities; i++)
         {
             for(int j = 1; j < numCities; j++)
             {
                 if(i != j)
                 {
-                    ArrayList<Integer> testCase = cities;
+                    ArrayList<Integer> testCase = new ArrayList<Integer>(cities);
                     int temp = testCase.get(i);
                     testCase.set(i, testCase.get(j));
                     testCase.set(j, temp);
                     int testCost = calcCost(testCase);
-                    if(bestCost == 0 || bestCost > testCost)
+                    if(testCost < bestCost)
                     {
                         bestCost = testCost;
                         bestSwap[0] = i;
@@ -46,14 +62,33 @@ public class HillClimbing
         return bestSwap;
     }
     
-    public int calcCost(ArrayList testCase)
+    public boolean completeSwap(int[] bestSwap)
+    {
+        if(bestSwap[0] != bestSwap[1])
+        {
+            System.out.println("Swapping " + tour.get(bestSwap[0]) + " and " + tour.get(bestSwap[1]));
+            int oldCost = calcCost(tour);
+            System.out.println("Old cost: " + oldCost);
+            int temp = tour.get(bestSwap[0]);
+            tour.set(bestSwap[0], tour.get(bestSwap[1]));
+            tour.set(bestSwap[1], temp);
+            int newCost = calcCost(tour);
+            System.out.println("Completed swap, new tour: " + tour);
+            System.out.println("New cost: " + newCost);
+            return true;
+        }
+        else
+            return false;
+        
+    }
+    
+    public int calcCost(ArrayList<Integer> testCase)
     {
         int cost = 0;
-        for(int i = 0; i < numCities - 1; i++)
+        for(int i = 0; i < testCase.size() - 1; i++)
         {
-            cost = cost + hcGraph.getCost(i, i + 1); //i should be y/From, i+1 should be x/To
+            cost = cost + hcGraph.getCost(testCase.get(i), testCase.get(i + 1)); //i should be y/From, i+1 should be x/To
         }
-        cost = cost + hcGraph.getCost(numCities - 1, 0);
         return cost;
     }
 }
