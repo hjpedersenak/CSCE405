d46e2a85f3e3dcd2fbf8b4ce74c6dee593ae66c3
hjpedersenak <32372147+hjpedersenak@users.noreply.github.com>
2018-10-04 17:39:55 -0800

    Update HillClimbing.java

--- TravelingSalesperson/src/travelingsalesman/HillClimbing.java
+++ TravelingSalesperson/src/travelingsalesman/HillClimbing.java
@@ -31,7 +31,6 @@ public class HillClimbing
         System.out.println("Final tour: " + tour + "\nFinal cost: " + newCost);
         return newCost;
     }
-<<<<<<< HEAD
     
     public int[] swapTest(ArrayList<Integer> cities)
     {
@@ -82,58 +81,6 @@ public class HillClimbing
         
     }
     
-=======
-    
-    public int[] swapTest(ArrayList<Integer> cities)
-    {
-        int[] bestSwap = new int[2];
-        bestSwap[0] = 0;
-        bestSwap[1] = 0;
-        int bestCost = calcCost(cities);
-        for(int i = 1; i < numCities; i++)
-        {
-            for(int j = 1; j < numCities; j++)
-            {
-                if(i != j)
-                {
-                    ArrayList<Integer> testCase = new ArrayList<Integer>(cities);
-                    int temp = testCase.get(i);
-                    testCase.set(i, testCase.get(j));
-                    testCase.set(j, temp);
-                    int testCost = calcCost(testCase);
-                    if(testCost < bestCost)
-                    {
-                        bestCost = testCost;
-                        bestSwap[0] = i;
-                        bestSwap[1] = j;
-                    }
-                }
-            }
-        }
-        return bestSwap;
-    }
-    
-    public boolean completeSwap(int[] bestSwap)
-    {
-        if(bestSwap[0] != bestSwap[1])
-        {
-            System.out.println("Swapping " + tour.get(bestSwap[0]) + " and " + tour.get(bestSwap[1]));
-            int oldCost = calcCost(tour);
-            System.out.println("Old cost: " + oldCost);
-            int temp = tour.get(bestSwap[0]);
-            tour.set(bestSwap[0], tour.get(bestSwap[1]));
-            tour.set(bestSwap[1], temp);
-            int newCost = calcCost(tour);
-            System.out.println("Completed swap, new tour: " + tour);
-            System.out.println("New cost: " + newCost);
-            return true;
-        }
-        else
-            return false;
-        
-    }
-    
->>>>>>> HannaBranch
     public int calcCost(ArrayList<Integer> testCase)
     {
         int cost = 0;
@@ -143,9 +90,4 @@ public class HillClimbing
         }
         return cost;
     }
-<<<<<<< HEAD
 }
-
-=======
-}
->>>>>>> HannaBranch
