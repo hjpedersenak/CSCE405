501bc53d70f72757f31e6da6913b5252f7b586c9
Hanna <hannajane.p@gmail.com>
2018-10-04 16:30:48 -0800

    working on simulated annealing

--- TravelingSalesperson/src/travelingsalesman/Annealing.java
+++ TravelingSalesperson/src/travelingsalesman/Annealing.java
@@ -33,59 +33,58 @@ public class Annealing {
         return cost;
     }
 
-    public void simulatedAnnealing(ArrayList<Integer> currentSolution) {
-//        snGraph.getRandomStart();
-//        currentSolution = snGraph.makeTour(snGraph);
+    public int simulatedAnnealing(ArrayList<Integer> currentSolution) {
+    
+        System.out.println("Start Cost: " + calcCost(currentSolution));
         
-        ArrayList<Integer> best = new ArrayList<Integer>();
-        best = snGraph.makeTour(snGraph);
-
+        ArrayList<Integer> best = new ArrayList<Integer>(currentSolution);
+        int startIndex = 1;
+        int endIndex = snGraph.getArrSize() - 1;
+        Collections.shuffle(best.subList(1, snGraph.getArrSize() - 1));
+       
         while (temp > 1) {
+            
+            Random rand = new Random();
  
-            ArrayList<Integer> newTour = new ArrayList<Integer>();
-            newTour = snGraph.makeTour(snGraph);
-            newTour = new ArrayList<Integer>(newTour.subList(1, newTour.size() - 1));
+            ArrayList<Integer> newSolution = new ArrayList<Integer>(currentSolution);
+            
+//            Collections.shuffle(newSolution.subList(1, snGraph.getArrSize() - 1));
+//            System.out.println("New solution: " + newSolution);
 
-            int tourPos1 = (int) (numCities * Math.random());
-            int tourPos2 = (int) (numCities * Math.random());
-
-            int citySwap1 = tourPos1;
-            int citySwap2 = tourPos2;
-
+            int tourPos1 = rand.nextInt(newSolution.size() - 2) + 1;
+            int tourPos2 = rand.nextInt(newSolution.size() -2) + 1;
+           
             for (int i = 1; i < numCities; i++) {
                 for (int j = 1; j < numCities; j++) {
                     if (i != j) {
-                        ArrayList<Integer> test = new ArrayList<Integer>(currentSolution);
-                        int temp = test.get(tourPos1);
-                        test.set(tourPos1, tourPos2);
-                        test.set(tourPos2, temp);
+
+                            int temp = newSolution.get(tourPos1);
+                            newSolution.set(tourPos1, newSolution.get(tourPos2));
+                            newSolution.set(tourPos2, temp);
                     }
                 }
             }
 
             int currentEnergy = calcCost(currentSolution);
-            int neighborEnergy = calcCost(newTour);
+            int neighborEnergy = calcCost(newSolution);
             
             // accept neighbor? 
             if (acceptanceProbability(currentEnergy, neighborEnergy, temp) > Math.random()) {
-                currentSolution = snGraph.makeTour(snGraph);
-//                currentSolution = new ArrayList<Integer>(currentSolution.subList(1, currentSolution.size() - 1));
+                currentSolution = newSolution;
             }
             
             // keep track of best solution
             if (calcCost(currentSolution) < calcCost(best)) {
-                best = snGraph.makeTour(snGraph);
-//                best = new ArrayList<Integer>(best.subList(1, best.size() - 1));
+                best = best = new ArrayList<Integer>(currentSolution);
             }
-            
-            temp *= 1-coolingRate;
-            
-            System.out.println("Temp: " + temp);
 
+            temp *= 1-coolingRate;
         }
+      
+        System.out.println("Final Cost: " + calcCost(best) + "\n");
+//        System.out.println("Final Tour: " + best +"\n");
         
-        System.out.println("Final solution distance: " + calcCost(best));
-        System.out.println("Final Tour: " + best);
+        return calcCost(best);
         
     }
 
