a77b0d27928ef7c490521ee5a8a0d521f5242285
Hanna <hannajane.p@gmail.com>
2018-10-04 16:50:39 -0800

    Fixed algorithm logic, just recording incorrectly in main array

--- TravelingSalesperson/src/travelingsalesman/Annealing.java
+++ TravelingSalesperson/src/travelingsalesman/Annealing.java
@@ -34,13 +34,16 @@ public class Annealing {
     }
 
     public int simulatedAnnealing(ArrayList<Integer> currentSolution) {
+        
+        System.out.println("\n\n\n\n\n\n\n");
     
-        System.out.println("Start Cost: " + calcCost(currentSolution));
+        System.out.println("\nStart Cost: " + calcCost(currentSolution));
+        System.out.println("\nStart tour: " + currentSolution);
         
         ArrayList<Integer> best = new ArrayList<Integer>(currentSolution);
-        int startIndex = 1;
-        int endIndex = snGraph.getArrSize() - 1;
-        Collections.shuffle(best.subList(1, snGraph.getArrSize() - 1));
+//        int startIndex = 1;
+//        int endIndex = snGraph.getArrSize() - 1;
+//        Collections.shuffle(best.subList(1, snGraph.getArrSize() - 1));
        
         while (temp > 1) {
             
@@ -49,21 +52,23 @@ public class Annealing {
             ArrayList<Integer> newSolution = new ArrayList<Integer>(currentSolution);
             
 //            Collections.shuffle(newSolution.subList(1, snGraph.getArrSize() - 1));
-//            System.out.println("New solution: " + newSolution);
+            if (temp < 1.5)
+                System.out.println("New solution: " + newSolution);
 
             int tourPos1 = rand.nextInt(newSolution.size() - 2) + 1;
             int tourPos2 = rand.nextInt(newSolution.size() -2) + 1;
            
-            for (int i = 1; i < numCities; i++) {
-                for (int j = 1; j < numCities; j++) {
-                    if (i != j) {
+//            for (int i = 1; i < newSolution.size(); i++) {
+//                for (int j = 1; j < newSolution.size(); j++) {
 
-                            int temp = newSolution.get(tourPos1);
+                            int tem = newSolution.get(tourPos1);
                             newSolution.set(tourPos1, newSolution.get(tourPos2));
-                            newSolution.set(tourPos2, temp);
-                    }
-                }
-            }
+                            newSolution.set(tourPos2, tem);
+//                }
+//            }
+            
+            if (temp < 1.5)                
+                System.out.println("New solution after swap: " + newSolution);
 
             int currentEnergy = calcCost(currentSolution);
             int neighborEnergy = calcCost(newSolution);
@@ -75,17 +80,19 @@ public class Annealing {
             
             // keep track of best solution
             if (calcCost(currentSolution) < calcCost(best)) {
-                best = best = new ArrayList<Integer>(currentSolution);
+                best = new ArrayList<Integer>(currentSolution);
             }
+            
+            if (temp < 1.5) 
+                System.out.println("Temperature: " + temp);
 
             temp *= 1-coolingRate;
         }
       
-        System.out.println("Final Cost: " + calcCost(best) + "\n");
+        System.out.println("FINAL COST: " + calcCost(best) + "\n");
 //        System.out.println("Final Tour: " + best +"\n");
         
         return calcCost(best);
-        
     }
 
 }
