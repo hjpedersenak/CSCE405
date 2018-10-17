67c644846402920c9c2d0c7b945932fa35da27e8
Nicolas Wibisono <ncwibisono@alaska.edu>
2018-10-16 18:08:29 -0800

    finished basic functionality of Owari.

--- Owari/src/owari/Game.java
+++ Owari/src/owari/Game.java
@@ -1,26 +1,37 @@
-
 package owari;
+
 import java.util.Scanner;
 
-public class Game 
-{
-    public static void main(String[] args) 
-    {
-       Scanner keyboard = new Scanner(System.in); 
-       String mode, whoMoveFirst;
-       System.out.println("Welcome to Owari! \nEnter 1 for single player, enter 2 for 2 players mode.");
-       mode = keyboard.nextLine();
-       System.out.println("Enter 1 if you want to play first, 2 if you want to play second"); //
-       whoMoveFirst = keyboard.nextLine();
-       if(mode.equals("1"))
-       {
-           // play against AI minimax
-       }
-       else
-       {
-           // play against other player (local)
-       }
-       
+public class Game {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String mode;
+        int whoMoveFirst;
+        System.out.println("Welcome to Owari!");
+        // play against AI minimax
+        System.out.println("Enter 1 if you want to play first, 2 if you want to play second"); //
+        whoMoveFirst = keyboard.nextInt();
+        keyboard.nextLine();
+        Owari owari = new Owari(whoMoveFirst);
+        owari.printBoard();
+
+        while (!owari.endGame()) {
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Player " + owari.getCurrentPlayer() + ", enter your selected pitNumber (0-5): ");
+                int pitNumber = keyboard.nextInt();
+                keyboard.nextLine();
+                while (pitNumber > 5 || pitNumber < 0 || owari.getBoard(owari.getCurrentPlayer(), pitNumber) == 0) { // check for out of bound and empty pits
+                    System.out.println("INVALID MOVE, enter your selected pitNumber (0-5)");
+                    pitNumber = keyboard.nextInt();
+                    keyboard.nextLine();
+                }
+
+                owari.moveStones(owari.getCurrentPlayer(), pitNumber);
+                owari.switchTurn();
+                owari.printBoard();
+            }
+        }
     }
-    
+
 }
--- Owari/src/owari/Owari.java
+++ Owari/src/owari/Owari.java
@@ -1,14 +1,181 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package owari;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Owari 
-{
-
-
-    public static void main(String[] args) 
+/**
+ *
+ * @author Nicolas Wibisono
+ */
+public class Owari {
+    
+    private int [][] board;
+    private int boardLength = 7;
+    private int currentPlayer;
+    private int NORTH = 0;
+    private int SOUTH = 1;
+    private int totalPlayers = 2;
+    private int AITurn;
+    private int playerTurn;
+    private int goalPit = 6;
+    
+    public Owari(int playerTurn)
     {
-       
+        this.playerTurn = playerTurn-1;
+        if(playerTurn == 1){
+            AITurn = 0;
+        }
+        else{
+            AITurn = 1;
+        }
+        currentPlayer = 0;
+
+        
+        
+        board = new int [totalPlayers][boardLength];
+        for(int i = 0; i < totalPlayers; i++)
+        {
+            for(int j = 0; j < boardLength; j++)
+            {
+                if(j == (boardLength - 1))
+                    board[i][j] = 0;
+                else
+                    board[i][j] = 3;
+            }
+        }
     }
     
+    public void printBoard()
+    {
+        // NORTH is reversed, so pit number 1 is the most right pit
+        System.out.print(" ");
+        for(int i = boardLength - 2; i >= 0; i--)
+        {
+            System.out.print(board[NORTH][i]);
+        }
+        System.out.println("\n" + board[NORTH][boardLength-1] + "      " + board[SOUTH][boardLength-1]);
+        System.out.print(" ");
+        for(int i = 0; i < boardLength - 1; i++)
+        {
+            System.out.print(board[SOUTH][i]);
+        }
+        System.out.println();
+    }
+    
+    public int getBoard(int side, int pitNumber) {
+        return board[side][pitNumber];
+    }
+
+    public void setBoard(int side, int pitNumber, int value) {
+        this.board[side][pitNumber] = value;
+    }
+    
+    public int getOpposite(int side)
+    {
+        if(side == NORTH)
+            return SOUTH;
+        return NORTH;
+    }
+    
+    public void moveStones(int side, int pitNumber)
+    {
+        int stoneInHand = board[side][pitNumber];
+        board[side][pitNumber] = 0;
+        int currentPit = pitNumber;
+        int currentSide = side;
+        for(;stoneInHand > 0; stoneInHand--)
+        {
+            currentPit++; // move to next pit (counter clockwise)
+            if (checkPit(currentSide,currentPit))
+            {
+               currentSide = getOpposite(currentSide);
+               currentPit = 0;
+            }
+            board[currentSide][currentPit]++;
+//                if (getBoard(currentSide, currentPit) > 1 && currentPit != goalPit) //ignore this part, probably wrong. 
+//            {
+//                stoneInHand = getBoard(currentSide, currentPit);
+//                setBoard(currentSide, currentPit, 0);
+//            }
+        }
+        checkCapture(currentPit, currentSide);
+    }
+    
+    public boolean checkCapture(int lastPit, int currentSide)
+    {
+        int oppositePit = getOppositePit(lastPit);
+        if(currentSide == currentPlayer && lastPit != goalPit && getBoard(currentPlayer, lastPit) == 1 && getBoard(getOpposite(currentPlayer), oppositePit) > 0)
+        {
+            board[currentPlayer][goalPit]+= getBoard(getOpposite(currentPlayer), oppositePit);
+            setBoard(getOpposite(currentPlayer), oppositePit, 0);
+            return true;
+        }
+        return false;
+    }
+    
+    private int getOppositePit(int pitNumber) // I am too stupid to do it any other way
+    {
+        switch(pitNumber)
+        {
+            case 0:
+                return 5;
+            case 1:
+                return 4;
+            case 2:
+                return 3;
+            case 3:
+                return 2;
+            case 4:
+                return 1;
+        }
+        return 0;
+    }
+    
+    public boolean checkPit(int side, int pitNumber)
+    {
+        if(side != currentPlayer && pitNumber == goalPit) // if the current player not on their own side, do not put stone in other player goalPit
+            return true;
+        if(pitNumber > goalPit) // prevent out of bound
+            return true;
+        return false;
+    }
+    
+    public int getCurrentPlayer()
+    {
+        return currentPlayer;
+    }
+    
+    public int switchTurn()
+    {
+        if(currentPlayer == NORTH)
+            currentPlayer = SOUTH;
+        else 
+            currentPlayer = NORTH;
+        return currentPlayer;
+    }
+    
+    public String getSideString(int side)
+    {
+        if(side == NORTH)
+            return "NORTH";
+        return "SOUTH";
+    }
+    
+    public boolean endGame()
+    {
+        for(int i = 0; i < totalPlayers; i++)
+        {
+            for(int j = 0; j < goalPit; j++)
+            {
+                if(getBoard(i, j) > 0)
+                    return false;
+            }
+        }
+        return true;
+    }
 }
