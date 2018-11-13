8951bac78e8a5064b5f7ed5ed525a44ca029afd1
Olivia Putman <ojputman@alaska.edu>
2018-11-12 22:39:07 -0900

    altered print slightly

--- Sudoku/src/sudoku/Board.java
+++ Sudoku/src/sudoku/Board.java
@@ -26,8 +26,16 @@ public class Board
             for(int row = 0; row < 9; row++)
             {
                 System.out.print(fullBoard[column][row] + " ");
+                if(row == 2 || row == 5)
+                {
+                    System.out.print("| ");
+                }
             }
             System.out.println();
+            if(column == 2 || column == 5)
+            {
+                //not sure how to print a nice horizontal divider yet
+            }
         }
     }
 }
