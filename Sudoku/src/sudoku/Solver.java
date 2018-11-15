85cd46a02d172c0c92f4d777e9b0fbf153c265b1
Hanna <hannajane.p@gmail.com>
2018-11-14 15:38:58 -0900

    starting to work on solver

--- Sudoku/src/sudoku/Solver.java
+++ Sudoku/src/sudoku/Solver.java
@@ -1,8 +1,34 @@
 
 package sudoku;
 
-
 public class Solver 
 {
+    String input;
+//     Board board = new Board(input);
     
+    public Board solveBoard(Board board) {
+        
+        return board;
+    }
+    
+    // check if row, column and 3X3 are valid 
+    public boolean isValid(Board board) {
+        
+    }
+    
+    public boolean rowConstraint(Board board, int row) {
+        
+    }
+    
+    public boolean columnConstraint(Board board, int column) {
+        
+    }   
+    
+    public boolean threeByThreeConstraint(Board board, int row, int column) {
+        
+    }
+    
+    public boolean checkConstraint() {
+        
+    }
 }
